445d59af6a2e2da5815bffc61e984932dc80a444
Anna Golasowska <annagolasowska@gmail.com>
2017-12-20 20:27:10 +0100

    added Suggestions with button

--- src/Suggestion.js
+++ src/Suggestion.js
@@ -6,24 +6,28 @@ export default class Suggestion extends React.Component{
   constructor(props){
     super(props);
     this.state={
+      loadingIbu: false,
+      loadingAbv: false,
+      loadingEbc: false,
       sugIbu: [],
       sugAbv: [],
       sugEbc: []
     }
   };
 
-  componentDidMount=()=>{
-      this.FetchIbu();
-      this.FetchAbv();
-      this.FetchEbc();
-  };
+  showSuggestion=()=>{
+    this.FetchIbu();
+    this.FetchAbv();
+    this.FetchEbc();
+  }
 
   FetchIbu=()=>{
-    const {ibu}=this.props.detailData;
+    const ibu=Math.floor(this.props.detailData.ibu);
     axios.get(`https://api.punkapi.com/v2/beers?ibu_gt=${ibu-1}&ibu_lt=${ibu+1}`)
     .then(response => {
       this.setState({
-        sugIbu: response.data[1]
+        sugIbu: response.data[1],
+        loadingIbu: true
       })
     })
     .catch(function(error) {
@@ -36,7 +40,8 @@ export default class Suggestion extends React.Component{
     axios.get(`https://api.punkapi.com/v2/beers?abv_gt=${abv-1}&abv_lt=${abv+1}`)
     .then(response => {
       this.setState({
-        sugAbv: response.data[1]
+        sugAbv: response.data[1],
+        loadingAbv: true
       })
     })
     .catch(function(error) {
@@ -45,11 +50,12 @@ export default class Suggestion extends React.Component{
   };
 
   FetchEbc=()=>{
-    const {ebc}=this.props.detailData;
+    const ebc=Math.floor(this.props.detailData.ebc);
     axios.get(`https://api.punkapi.com/v2/beers?ebc_gt=${ebc-1}&ebc_lt=${ebc+1}`)
     .then(response => {
       this.setState({
-        sugEbc: response.data[1]
+        sugEbc: response.data[1],
+        loadingEbc: true
       })
     })
     .catch(function(error) {
@@ -57,15 +63,13 @@ export default class Suggestion extends React.Component{
     });
   };
 
-
-
   render(){
 
-    return(
-      <div>
-        <BeerItem beer={this.state.sugIbu} />
-        <BeerItem beer={this.state.sugAbv} />
-        <BeerItem beer={this.state.sugEbc} />
+    return(<div>
+      <button onClick={this.showSuggestion}>Pokaz podobne piwerka</button>
+      {this.state.loadingIbu?(<BeerItem beer={this.state.sugIbu} />):null}
+      {this.state.loadingIbu?(<BeerItem beer={this.state.sugAbv} />):null}
+      {this.state.loadingIbu?(<BeerItem beer={this.state.sugEbc} />):null}
       </div>
     )
   }
