f282092d933ab883521e44b59d349f36b2a90461
Anna Golasowska <annagolasowska@gmail.com>
2017-12-20 13:08:48 +0100

    added items to and displayed items in Favourite

--- src/FavouriteBeer.js
+++ src/FavouriteBeer.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import Navigation from './Navigation.js';
-import BeerItem from './BeerItem';
+import BeerItem from './BeerItem.js';
 
 export default class FavouriteBeer extends React.Component{
 
 
+  displayBeers =()=>{
+    const beers = JSON.parse(localStorage.getItem('myFavBeers')) || [];
+    return beers.map(beer=>{
+      return <BeerItem key={beer.id} beer={beer} />
 
-
-
+    })
+  };
 
 
   render(){
-
     return(
         <div>
-          <Navigation/>
+          <Navigation />
+          <div>
+            {this.displayBeers()}
+          </div>
         </div>
     )
   }
